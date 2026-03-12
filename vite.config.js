5178fc650303fe5edbb682828cd3c7c36b712887
DuyKhang <khangtdce181439@fpt.edu.vn>
2026-03-12 13:21:13 +0700

    Fix Google Login COOP issue

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    // Thêm đoạn này để sửa lỗi COOP
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
     proxy: {
-      '/api': {
-        target: 'https://tramcuuho.onrender.com',
+      "/api": {
+        target: "https://tramcuuho.onrender.com",
         // target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
